5a91a3be043e36a2ee65d2a28af41d25414165e3
suhong <67825309+tnghd5761@users.noreply.github.com>
2021-02-15 23:57:55 +0900

    Feb15_practice
    table, form 등 태그 공부

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+          <meta charset="utf-8"></meta>
       </header>
+      <body>
+        <form action="http://localhost/login.php">
+            <p>아이디 : <input type="text" name="id"></input></p>
+            <p>비밀번호 : <input type="password" name="pwd"></input></p>
+            <p>주소 : <input type="text" name="address"></input></p>
+            <input type="submit"></input>
+        </form>
+      </body>
     </div>
   );
 }
